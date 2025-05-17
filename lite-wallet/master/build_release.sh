52a311e7465e84aac89b5615e66c209991e0f6bf
cryptoforge <cryptoforge.cc@gmail.com>
2025-05-17 09:26:37 -0700

    Update lite wallet

--- lite-wallet/master/build_release.sh
+++ lite-wallet/master/build_release.sh
@@ -13,4 +13,11 @@ docker buildx prune --all --force
 docker system df
 cd ..
 
+cd ubuntu20.04_aarch64_cc
+docker system df
+time docker build --no-cache -o . --target=binaries .
+docker buildx prune --all --force
+docker system df
+cd ..
+
 
